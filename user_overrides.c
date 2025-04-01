2bb96274cc4366185681a483226fd8834eac2097
kjuq <44694396+kjuq@users.noreply.github.com>
2025-04-01 10:14:45 +0900

    mod: Ctrl-u for Linux

--- user_overrides.c
+++ user_overrides.c
@@ -73,8 +73,8 @@ key_override_t cmd_q_override = ko_make_basic(MOD_BIT(KC_LGUI), KC_Q, LGUI(KC_GR
 key_override_t ctrl_tab_override  = ko_make_basic(MOD_BIT(KC_LCTL), KC_ESC, LCTL(KC_TAB));
 key_override_t alt_tab_override   = ko_make_basic(MOD_BIT(KC_RALT), KC_ESC, RALT(KC_TAB));
 key_override_t cmd_tab_override   = ko_make_basic(MOD_BIT(KC_LGUI), KC_ESC, LGUI(KC_TAB));
-key_override_t shift_tab_override = ko_make_basic(MOD_BIT(KC_RSFT), KC_ESC, RSFT(KC_TAB));
-key_override_t ctrl_u_key_override = ko_make_basic(MOD_BIT(KC_LCTL), KC_U, LCMD(KC_BSPC));
+key_override_t shift_tab_override = ko_make_basic(MOD_BIT(KC_RSFT), KC_ESC, LSFT(KC_TAB));
+key_override_t ctrl_u_key_override = ko_make_basic(MOD_BIT(KC_LCTL), KC_U, LSFT(RCTL(KC_BSPC)));
 
 key_override_t hrm_right_key_override = ko_make_basic(MOD_BIT(KC_LCTL), CM_F, KC_RIGHT);
 key_override_t hrm_left_key_override  = ko_make_basic(MOD_BIT(KC_LCTL), CM_B, KC_LEFT);
@@ -96,8 +96,8 @@ key_override_t hrm_cmd_q_override = ko_make_basic(MOD_BIT(KC_LGUI), CM_Q, LGUI(K
 key_override_t hrm_ctrl_tab_override  = ko_make_basic(MOD_BIT(KC_LCTL), CM_ESC, LCTL(KC_TAB));
 key_override_t hrm_alt_tab_override   = ko_make_basic(MOD_BIT(KC_RALT), CM_ESC, RALT(KC_TAB));
 key_override_t hrm_cmd_tab_override   = ko_make_basic(MOD_BIT(KC_LGUI), CM_ESC, LGUI(KC_TAB));
-key_override_t hrm_shift_tab_override = ko_make_basic(MOD_BIT(KC_RSFT), CM_ESC, RSFT(KC_TAB));
-key_override_t hrm_ctrl_u_key_override = ko_make_basic(MOD_BIT(KC_LCTL), CM_U, LCMD(KC_BSPC));
+key_override_t hrm_shift_tab_override = ko_make_basic(MOD_BIT(KC_RSFT), CM_ESC, LSFT(KC_TAB));
+key_override_t hrm_ctrl_u_key_override = ko_make_basic(MOD_BIT(KC_LCTL), CM_U, LSFT(RCTL(KC_BSPC)));
 
 key_override_t linux_a_override   = ko_make_basic(MOD_BIT(KC_LGUI), KC_A, LCTL(KC_A));
 key_override_t linux_b_override   = ko_make_basic(MOD_BIT(KC_LGUI), KC_B, LCTL(KC_B));
@@ -573,10 +573,18 @@ void reload_overrides() {
 		user_switch_override(&hrm_end_key_override, false);
 	}
 
-	if (user_is_linux() || user_is_windows() || !user_config.override_ctrl_u) {
+	if (user_is_macos() || user_is_ios()) {
+		ctrl_u_key_override.replacement = LCMD(KC_BSPC);
+		hrm_ctrl_u_key_override.replacement = LCMD(KC_BSPC);
+	} else if (user_is_linux() || user_is_windows()) {
+		ctrl_u_key_override.replacement = LSFT(RCTL(KC_BSPC));
+		hrm_ctrl_u_key_override.replacement = LSFT(RCTL(KC_BSPC));
+	}
+	if (!user_config.override_ctrl_u) {
 		user_switch_override(&ctrl_u_key_override, false);
 		user_switch_override(&hrm_ctrl_u_key_override, false);
 	}
+
 	if (user_is_linux() || user_is_windows() || !user_config.override_cmd_q) {
 		user_switch_override(&cmd_q_override, false);
 		user_switch_override(&hrm_cmd_q_override, false);
