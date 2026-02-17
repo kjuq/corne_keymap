f7fa06332c862628d33021e93e17eb6af3fea857
kjuq <44694396+kjuq@users.noreply.github.com>
2026-02-17 15:14:26 +0900

    Change keybinds of inner 4 keys

--- user_layouts.c
+++ user_layouts.c
@@ -10,8 +10,8 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_COLEMAK] = LAYOUT_split_3x5_3_ex2( // {{{
-		KC_ESC,  KC_W,    KC_F,    KC_P,    KC_B,    KC_DEL,          KC_PGUP, KC_J,    KC_L,    KC_U,    KC_Y,    KC_Q,
-		KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_ENT,          KC_PGDN, KC_H,    KC_N,    KC_E,    KC_I,    KC_O,
+		KC_ESC,  KC_W,    KC_F,    KC_P,    KC_B,    KC_PGUP,         KC_TAB,  KC_J,    KC_L,    KC_U,    KC_Y,    KC_Q,
+		KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_PGDN,         KC_ENT,  KC_H,    KC_N,    KC_E,    KC_I,    KC_O,
 		KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                              KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SCLN,
 		                           LOWER,   KC_LCTL, KC_SPC,          KC_SPC,  KC_RSFT, RAISE
 	), // }}}
